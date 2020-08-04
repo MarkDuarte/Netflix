daca0a2da1904b91d995f6d454cb7816043ef7af
Marcos Duarte <marcosduarte1994@gmail.com>
2020-08-04 13:27:01 -0300

    Commit 2

--- src/pages/cadastro/categoria/index.jsx
+++ src/pages/cadastro/categoria/index.jsx
@@ -1,21 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
+  const [categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
   return (
     <PageDefault>
-      <h1>Cadastro de vídeo.</h1>
-        <form>
-          <label>
-            Nome da Categoria:
-            <input type="text"
-            />
-          </label>
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+        <form onSubmit={function HandleSubmit(e) {
+          e.preventDefault();
+          setCategorias([
+            ...categorias,
+            nomeDaCategoria
+          ]);
+        }}>
+          <div>
+            <label>
+              Nome da Categoria:
+              <input 
+                type="text"
+                value={nomeDaCategoria}
+                onChange={(e) => setNomeDaCategoria(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Descrição:
+              <textarea 
+                type="text"
+                value={nomeDaCategoria}
+                onChange={(e) => setNomeDaCategoria(e.target.value)}
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Cor:
+              <input 
+                type="color"
+                value={nomeDaCategoria}
+                onChange={(e) => setNomeDaCategoria(e.target.value)}
+              />
+            </label>
+          </div>
+
           <button>
             Cadastrar
           </button>
         </form>
+
+        <ul>
+          {categorias.map((categoria, index) => {
+            return (
+              <li key={index}>
+                {`${categoria}${index}`}
+              </li>
+            )
+          })}
+        </ul>
         
       <Link to="/">
         Ir para a home.
