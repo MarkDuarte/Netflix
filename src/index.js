91ea60eb99ba6f5744ef5c6fe07d9b5075dee59c
Marcos Duarte <marcosduarte1994@gmail.com>
2020-08-03 22:59:03 -0300

    Parte 2

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from '../src/pages/home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from '../src/pages/cadastro/video';
+import CadastroCategoria from '../src/pages/cadastro/categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/Cadastro/Video" component={CadastroVideo} />
+      <Route path="/Cadastro/Categoria" component={CadastroCategoria} />
+      <Route component={() => (<div>404: Página não encontrada.</div>)} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
